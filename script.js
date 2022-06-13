35745b7388e7d1abe75636f2ac8cfb34d32c32ea
supertrupert <37668856+supertrupert@users.noreply.github.com>
2022-06-13 14:05:37 +0100

    Update script.js
    Added comments

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-// Load coins table and coin symbols
+// Load coins table and fields "Symbol" and "Latest price"
 const coinsTable = base.getTable("Coins");
 const coinsQuery = await coinsTable.selectRecordsAsync({
     fields: ["Symbol", "Latest price"]
 });
 
+// Build a string of coin symbols to be used in our request url
 const coinsSymbolsArr = coinsQuery.records.map(rec => rec.getCellValue("Symbol"));
 const coinsSymbolsStr = coinsSymbolsArr.join(',');
 
-// Fetch latest data for symbols
+// Fetch latest data for symbols (invalid coins will be ignored) and convert to EUR
 let response = await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=' + coinsSymbolsStr + '&skip_invalid=true' + '&convert=EUR',{
     headers: {
-        "X-CMC_PRO_API_KEY": "INSERT_YOUR_OWN_KEY"
+        "X-CMC_PRO_API_KEY": "3c5331eb-e68f-4324-8c8e-eefcec038f26"
     }        
 });
 
+// only continue if fetching data was a success (status 200)
 if(response.status === 200) {
     const data = await response.json();
 
+    // In order to avoid an error for incorrect / unavailable symbols, 
+    // we're extracting which coins have actually been fetched
     const listOfResponseCoins = Object.keys(data.data).join(',');
 
-    // Build update array 
+    // Build update array by filtering the list of coins for those that have been
+    // fetched and mapping in order to create the right format for updateRecordsAsync()
     const updateArr = coinsQuery.records
     .filter(rec => listOfResponseCoins.includes(rec.getCellValue("Symbol")))
     .map(rec => {
@@ -27,12 +32,14 @@ if(response.status === 200) {
         const latestPrice = data.data[rec.getCellValue("Symbol")].quote.EUR.price;
 
         return {
-            id: rec.id, 
+            id: id, 
             fields: {
                 "Latest price": latestPrice,
             }
         }
     });
+
+    // updating the table (this will fail for more than 50 records at a time)
     if(updateArr.length > 0) {
         await coinsTable.updateRecordsAsync(updateArr);
     }
